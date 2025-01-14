0871a810723b66ad461a5ad5bdfdbda863180a07
Xu Qian <1464238196@qq.com>
2025-01-15 03:43:18 +0800

    update animate

--- windows/front/app.tsx
+++ windows/front/app.tsx
@@ -1,9 +1,15 @@
 import { createRoot } from "react-dom/client";
 import skyStriker from "./assets/sky-striker.png";
 import "./Card.css";
+import "./Holo.css";
 
 const App = () => {
-  return <img src={skyStriker} className="ygo-card" />;
+  return (
+    <div>
+      <img src={skyStriker} className="ygo-card" />;
+      <div className="cardShinePlusAfterElement" />;
+    </div>
+  );
 };
 
 const root = createRoot(document.body);
