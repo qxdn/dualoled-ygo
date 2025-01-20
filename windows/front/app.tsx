aa4ee1336c512b29a5e2b7ba795f9ae314664355
Xu Qian <1464238196@qq.com>
2025-01-21 03:08:04 +0800

    card auto change size

--- windows/front/app.tsx
+++ windows/front/app.tsx
@@ -2,12 +2,49 @@ import { createRoot } from "react-dom/client";
 import skyStriker from "./assets/sky-striker.png";
 import "./Card.css";
 import "./Holo.css";
+import { useEffect, useState } from "react";
+
+// 获取窗口大小
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
 
 const App = () => {
+  const { height, width } = useWindowDimensions();
+
+  const styles = {
+    "--aspect-ratio": width / height,
+    width: `${width}px`,
+    top: 0,
+    left: 0,
+    position: "absolute",
+  };
+
   return (
     <div>
-      <img src={skyStriker} className="ygo-card" />;
-      <div className="cardShinePlusAfterElement" />;
+      <img src={skyStriker} className="ygo-card" />
+      <div className="cardShinePlusAfterElement" style={styles} />
     </div>
   );
 };
